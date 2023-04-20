a2e5350666d45e51787c699bb1589a211239d974
aryaantony92 <arya.antony@pimcore.com>
2023-04-20 15:18:24 +0200

    fix test

--- tests/Support/Helper/Unit.php
+++ tests/Support/Helper/Unit.php
@@ -15,23 +15,5 @@ class Unit extends \Codeception\Module
     public function _beforeSuite($settings = [])
     {
 
-        /** @var Pimcore $pimcoreModule */
-        $pimcoreModule = $this->getModule('\\' . Pimcore::class);
-
-        //create migrations table in order to allow installation - needed for SettingsStoreAware Installer
-
-        Db::get()->executeStatement('
-            create table migration_versions
-            (
-                version varchar(1024) not null
-                    primary key,
-                executed_at datetime null,
-                execution_time int null
-            )
-            collate=utf8_unicode_ci;
-            ');
-
-        $installer = $pimcoreModule->getContainer()->get(Installer::class);
-        $installer->install();
     }
 }
